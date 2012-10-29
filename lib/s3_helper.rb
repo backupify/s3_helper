6e4588f24bf89f1fed4857816018b8bc9d0359c0
Andrew Jorgensen <andrew@backupify.com>
2012-10-29 13:41:59 -0400

    collapsed class structure

--- lib/s3_helper.rb
+++ lib/s3_helper.rb
@@ -4,243 +4,239 @@ require 'exception_helper'
 require 'active_support/core_ext/object/try'
 require 'active_support/core_ext/object/blank'
 
-module S3
+class S3Helper
   class BlankBucketException < StandardError; end
   class BlankFileNameException < StandardError; end
+  include ExceptionHelper::Retry
 
-  class Helper
-    include ExceptionHelper::Retry
+  RETRYABLE_EXCEPTIONS = [Excon::Errors::Error]
+  MAX_KEYS = 1000
 
-    RETRYABLE_EXCEPTIONS = [Excon::Errors::Error]
-    MAX_KEYS = 1000
+  attr_accessor :directory
+  attr_reader :options
 
-    attr_accessor :directory
-    attr_reader :options
+  def initialize(bucket, options = {})
+    raise BlankBucketException.new unless bucket
+    @options = options
 
-    def initialize(bucket, options = {})
-      raise BlankBucketException.new unless bucket
-      @options = options
+    connect!(bucket)
+  end
 
-      connect!(bucket)
+  #data can be a string or io handle
+  def store(path, filename, data, opts={})
+    if data.respond_to?(:read)
+      multipart_store(path, filename, data, opts)
+    else
+      singlepart_store(path, filename, data, opts)
+    end
+  end
+
+  def singlepart_store(path, filename, data, opts={})
+    fullpath = validate_path(path, filename)
+    file = nil
+
+    # store the object
+    #logger.debug "S3 - Storing object: #{fullpath}"
+
+    retry_on_failure(*RETRYABLE_EXCEPTIONS) do
+      file = @directory.files.new(opts.merge(:key => fullpath))
+      file.body = data
+      file.save
     end
 
-    #data can be a string or io handle
-    def store(path, filename, data, opts={})
-      if data.respond_to?(:read)
-        multipart_store(path, filename, data, opts)
-      else
-        singlepart_store(path, filename, data, opts)
-      end
-    end
+    file
+  end
 
-    def singlepart_store(path, filename, data, opts={})
+  def multipart_store(path, filename, data, opts={})
+    opts = {:chunk_size => (5.megabytes)}.merge(opts)
+
+    chunk = data.read(opts[:chunk_size])
+
+    if chunk.nil? || (chunk.size < opts[:chunk_size])
+      singlepart_store(path, filename, chunk, opts)
+    else
       fullpath = validate_path(path, filename)
-      file = nil
+      #logger.info "S3 - Multipart uploading #{fullpath}"
 
-      # store the object
-      #logger.debug "S3 - Storing object: #{fullpath}"
+      part_ids = []
 
-      retry_on_failure(*RETRYABLE_EXCEPTIONS) do
-        file = @directory.files.new(opts.merge(:key => fullpath))
-        file.body = data
-        file.save
-      end
+      response = @storage.initiate_multipart_upload(@bucket, fullpath)
+      begin
+        upload_id = response.body['UploadId']
 
-      file
-    end
-
-    def multipart_store(path, filename, data, opts={})
-      opts = {:chunk_size => (5.megabytes)}.merge(opts)
-
-      chunk = data.read(opts[:chunk_size])
-
-      if chunk.nil? || (chunk.size < opts[:chunk_size])
-        singlepart_store(path, filename, chunk, opts)
-      else
-        fullpath = validate_path(path, filename)
-        #logger.info "S3 - Multipart uploading #{fullpath}"
-
-        part_ids = []
-
-        response = @storage.initiate_multipart_upload(@bucket, fullpath)
-        begin
-          upload_id = response.body['UploadId']
-
-          while chunk
-            next_chunk = data.read(opts[:chunk_size])
-            if data.eof?
-              chunk << next_chunk
-              next_chunk = nil
-            end
-            part_number = part_ids.size + 1
-            #logger.info("S3 - Uploading part #{part_number}")
-            retry_on_failure(*RETRYABLE_EXCEPTIONS) do
-              response = @storage.upload_part(@bucket, fullpath, upload_id, part_number, chunk)
-              part_ids << response.headers['ETag']
-            end
-            chunk = next_chunk
+        while chunk
+          next_chunk = data.read(opts[:chunk_size])
+          if data.eof?
+            chunk << next_chunk
+            next_chunk = nil
           end
+          part_number = part_ids.size + 1
+          #logger.info("S3 - Uploading part #{part_number}")
+          retry_on_failure(*RETRYABLE_EXCEPTIONS) do
+            response = @storage.upload_part(@bucket, fullpath, upload_id, part_number, chunk)
+            part_ids << response.headers['ETag']
+          end
+          chunk = next_chunk
+        end
 
-          @storage.complete_multipart_upload(@bucket, fullpath, upload_id, part_ids)
+        @storage.complete_multipart_upload(@bucket, fullpath, upload_id, part_ids)
           #logger.info("S3 - Completed multipart upload: #{upload_id}")
 
-        rescue Exception => e
-          #logger.error("S3 - Aborting multipart upload: #{upload_id}")
-          # don't want abort to fail, so reconnect to make sure fog
-          # storage instance not in a funky state
-          connect!(@bucket)
-          @storage.abort_multipart_upload(@bucket, fullpath, upload_id)
-          raise
-        end
+      rescue Exception => e
+        #logger.error("S3 - Aborting multipart upload: #{upload_id}")
+        # don't want abort to fail, so reconnect to make sure fog
+        # storage instance not in a funky state
+        connect!(@bucket)
+        @storage.abort_multipart_upload(@bucket, fullpath, upload_id)
+        raise
       end
     end
+  end
 
-    def fetch(path, filename, opts={}, &block)
-      fullpath = validate_path(path, filename)
-      if block_given?
-        connection.get_object(@bucket, fullpath, opts, &block)
-      else
-        begin
-          connection.get_object(@bucket, fullpath, opts).body
-        rescue Excon::Errors::NotFound
-          nil
-        end
+  def fetch(path, filename, opts={}, &block)
+    fullpath = validate_path(path, filename)
+    if block_given?
+      connection.get_object(@bucket, fullpath, opts, &block)
+    else
+      begin
+        connection.get_object(@bucket, fullpath, opts).body
+      rescue Excon::Errors::NotFound
+        nil
       end
     end
+  end
 
-    alias_method :stream, :fetch
+  alias_method :stream, :fetch
 
-    def head_object(path, filename, opts={})
-      fullpath = validate_path(path, filename)
+  def head_object(path, filename, opts={})
+    fullpath = validate_path(path, filename)
 
-      #logger.debug "S3 - Fetching head of object: " + fullpath
-      connection.head_object(@bucket, fullpath, opts).headers
+    #logger.debug "S3 - Fetching head of object: " + fullpath
+    connection.head_object(@bucket, fullpath, opts).headers
+  end
+
+  def head(path, filename, opts={})
+    fullpath = validate_path(path, filename)
+
+    @directory.files.head(fullpath, opts)
+  end
+
+  def delete(path, filename, opts={})
+    fullpath = validate_path(path, filename)
+
+    # store the object
+    #logger.debug "S3 - Deleting object: " + fullpath
+    retry_on_failure(*RETRYABLE_EXCEPTIONS) do
+      @directory.files.get(fullpath, opts).try(:destroy)
+    end
+  end
+
+  def rename(path, original_filename, new_filename)
+    original_file = @directory.files.get(validate_path(path, original_filename))
+
+    connection.copy_object(@directory.key, validate_path(path, original_filename), @directory.key, validate_path(path, new_filename))
+
+    original_file.destroy
+  end
+
+  def authenticated_url(path, filename, opts={})
+    opts = {:expires_in => 5.minutes}.merge(opts)
+    fullpath = validate_path(path, filename)
+
+    # Extract the expiration time from the options.
+    expiration_time = opts[:expires].present? ? opts[:expires] : Time.now + opts[:expires_in]
+    opts.delete(:expires)
+    opts.delete(:expires_in)
+
+    @directory.files.get_https_url(fullpath, expiration_time)
+  end
+
+  def connection
+    @directory.connection
+  end
+
+  def key
+    @directory.key
+  end
+
+  def storage_size(prefix)
+    sum = 0
+    each_s3_metadata_for_prefix(prefix) { |metadata| sum += metadata['Size'] }
+    sum
+  end
+
+  # return a hash of filename => file size given an s3 prefix
+  def file_sizes_for_prefix(prefix)
+    file_sizes_hash = {}
+    each_s3_metadata_for_prefix(prefix) { |metadata| file_sizes_hash[metadata['Key']] = metadata['Size'] }
+    file_sizes_hash
+  end
+
+  def each_s3_metadata_for_prefix(prefix)
+    marker = nil
+
+    loop do
+      response = @directory.connection.get_bucket(@directory.key, 'prefix' => prefix, 'max-keys' => MAX_KEYS, 'marker' => marker)
+
+      response.body['Contents'].each { |response_body| yield response_body }
+
+      marker = response.body['Contents'].last.try(:[], 'Key')
+
+      # IsTruncated will be false when we've retrieved all the items from S3.
+      break unless response.body['IsTruncated']
     end
 
-    def head(path, filename, opts={})
-      fullpath = validate_path(path, filename)
+    nil
+  end
 
-      @directory.files.head(fullpath, opts)
+  # returns a list no bigger than batch_size of files from s3 in a given directory (with a given prefix).
+  # does not currently support any sort of paging; it just returns the first [batch_size] files
+  def batch_directory_listing(prefix, batch_size = MAX_KEYS)
+    @directory.files.all('prefix' => prefix, 'max-keys' => batch_size)
+  end
+
+  def batch_directory_versions_listing(prefix, batch_size = MAX_KEYS)
+    @directory.versions.all('prefix' => prefix, 'max-keys' => batch_size)
+  end
+
+  def walk_tree(prefix=nil)
+    # Grab a new S3 connection since this method destroys the cached copy of files once the prefix is applied.
+    s3 = S3Helper.new(@directory.key, @options)
+
+    iter = s3.directory.files
+    iter.prefix = prefix if prefix
+
+    iter.each do |s3_file|
+      yield s3_file
     end
+  end
 
-    def delete(path, filename, opts={})
-      fullpath = validate_path(path, filename)
+  protected
 
-      # store the object
-      #logger.debug "S3 - Deleting object: " + fullpath
-      retry_on_failure(*RETRYABLE_EXCEPTIONS) do
-        @directory.files.get(fullpath, opts).try(:destroy)
-      end
-    end
+  def connect!(bucket)
+    @bucket = bucket
 
-    def rename(path, original_filename, new_filename)
-      original_file = @directory.files.get(validate_path(path, original_filename))
+    @storage_params = {:provider => 'AWS',
+                       :persistent => false,
+                       :aws_access_key_id => '',
+                       :aws_secret_access_key => '',
+                       :connection_options => {:retry_limit => 0}
+    }.merge(@options)
 
-      connection.copy_object(@directory.key, validate_path(path, original_filename), @directory.key, validate_path(path, new_filename))
+    @storage = Fog::Storage.new(@storage_params)
 
-      original_file.destroy
-    end
+    # This call looks misleading.  What we're doing is creating an in-memory representation of an existing bucket.
+    # The bucket :key will be used in operations related to interaction of files within the directory.
+    @directory = @storage.directories.new(:key => bucket)
+  end
 
-    def authenticated_url(path, filename, opts={})
-      opts = {:expires_in => 5.minutes}.merge(opts)
-      fullpath = validate_path(path, filename)
+  def validate_path(path, filename)
+    raise BlankFileNameException.new unless filename
 
-      # Extract the expiration time from the options.
-      expiration_time = opts[:expires].present? ? opts[:expires] : Time.now + opts[:expires_in]
-      opts.delete(:expires)
-      opts.delete(:expires_in)
+    fullpath = ""
+    fullpath = path + "/" if path.present?
+    fullpath = fullpath + filename
 
-      @directory.files.get_https_url(fullpath, expiration_time)
-    end
-
-    def connection
-      @directory.connection
-    end
-
-    def key
-      @directory.key
-    end
-    
-    def storage_size(prefix)
-      sum = 0
-      each_s3_metadata_for_prefix(prefix){ |metadata| sum += metadata['Size'] }
-      sum
-    end
-
-    # return a hash of filename => file size given an s3 prefix
-    def file_sizes_for_prefix(prefix)
-      file_sizes_hash = {}
-      each_s3_metadata_for_prefix(prefix){ |metadata| file_sizes_hash[metadata['Key']] = metadata['Size'] }
-      file_sizes_hash
-    end
-
-    def each_s3_metadata_for_prefix(prefix)
-      marker = nil
-
-      loop do
-        response = @directory.connection.get_bucket(@directory.key, 'prefix' => prefix, 'max-keys' => MAX_KEYS, 'marker' => marker)
-
-        response.body['Contents'].each { |response_body| yield response_body }
-
-        marker = response.body['Contents'].last.try(:[], 'Key')
-
-        # IsTruncated will be false when we've retrieved all the items from S3.
-        break unless response.body['IsTruncated']
-      end
-
-      nil
-    end
-
-    # returns a list no bigger than batch_size of files from s3 in a given directory (with a given prefix).
-    # does not currently support any sort of paging; it just returns the first [batch_size] files
-    def batch_directory_listing(prefix, batch_size = MAX_KEYS)
-      @directory.files.all('prefix' => prefix, 'max-keys' => batch_size)
-    end
-
-    def batch_directory_versions_listing(prefix, batch_size = MAX_KEYS)
-      @directory.versions.all('prefix' => prefix, 'max-keys' => batch_size)
-    end
-
-    def walk_tree(prefix=nil)
-      # Grab a new S3 connection since this method destroys the cached copy of files once the prefix is applied.
-      s3 = Helper.new(@directory.key, @options)
-
-      iter = s3.directory.files
-      iter.prefix = prefix if prefix
-
-      iter.each do |s3_file|
-        yield s3_file
-      end
-    end
-
-    protected
-
-    def connect!(bucket)
-      @bucket = bucket
-
-      @storage_params = {:provider => 'AWS',
-                         :persistent => false,
-                         :aws_access_key_id => '',
-                         :aws_secret_access_key => '',
-                         :connection_options => { :retry_limit => 0 }
-                        }.merge(@options)
-
-      @storage = Fog::Storage.new(@storage_params)
-
-      # This call looks misleading.  What we're doing is creating an in-memory representation of an existing bucket.
-      # The bucket :key will be used in operations related to interaction of files within the directory.
-      @directory = @storage.directories.new(:key => bucket)
-    end
-
-    def validate_path(path, filename)
-      raise BlankFileNameException.new unless filename
-
-      fullpath = ""
-      fullpath = path + "/" if path.present?
-      fullpath = fullpath + filename
-      
-      return fullpath
-    end
-
+    return fullpath
   end
 end
